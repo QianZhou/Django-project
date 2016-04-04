42b1b0e28a118b6b8f805c82451260cac0be8b3a
QianZhou <qzhou@lamar.edu>
2016-04-04 15:18:17 -0500

    Form Validation

--- trydjango18/src/newsletter/forms.py
+++ trydjango18/src/newsletter/forms.py
@@ -7,3 +7,21 @@ class SignUpForm(forms.ModelForm):
         model=SignUp
         fields=['full_name','email']
         ##exclude=['full_name'] usersparingly
+        
+    def clean_email(self):
+        email= self.cleaned_data.get('email')
+        email_base, provider=email.split("@")
+        domain,extension=provider.split('.')
+        if not domain =='UMD':
+            raise forms.ValidationError("Please use your UMD email address")
+  
+        if not extension=="edu":
+            raise forms.ValidationError("Please use a valid .EDU email address")
+        return email
+    
+    
+    def clean_full_name(self):
+        full_name=self.cleaned_data.get('full_name')
+        
+        return full_name
+    
