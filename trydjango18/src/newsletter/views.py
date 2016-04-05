77f2aa5b57d5c14a16b6bff349f27c30e10d6b24
QianZhou <qzhou@lamar.edu>
2016-04-04 19:41:02 -0500

    Custom Form in a View

--- trydjango18/src/newsletter/views.py
+++ trydjango18/src/newsletter/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import SignUpForm
+from .forms import ContactForm,SignUpForm
 
 # Create your views here.
 def home(request):
@@ -38,5 +38,20 @@ def home(request):
         context={
         "title":"Thank you"
         }
-    
     return render(request,"home.html",context)
+
+def contact(request):
+    form =ContactForm(request.POST or None)
+    if form.is_valid():
+        for key,value in form.cleaned_data.iteritems():
+            print key,value
+            #print form.cleaned_data.get(key)
+        #full_name=form.cleaned_data.get('full_name')
+        # email=form.cleaned_data.get('email')
+        #message=form.cleaned_data.get('message')
+        #print full_name,email,message
+        
+    context={
+          "form":form
+    }
+    return render(request,"forms.html",context)
