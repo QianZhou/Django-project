7a0818780e6500753ec00a473c44685f65196cc1
QianZhou <qzhou@lamar.edu>
2016-04-04 19:06:40 -0500

    Form in a View

--- trydjango18/src/newsletter/forms.py
+++ trydjango18/src/newsletter/forms.py
@@ -12,8 +12,8 @@ class SignUpForm(forms.ModelForm):
         email= self.cleaned_data.get('email')
         email_base, provider=email.split("@")
         domain,extension=provider.split('.')
-        if not domain =='UMD':
-            raise forms.ValidationError("Please use your UMD email address")
+        #if not domain =='UMD':
+            #raise forms.ValidationError("Please use your UMD email address")
   
         if not extension=="edu":
             raise forms.ValidationError("Please use a valid .EDU email address")
